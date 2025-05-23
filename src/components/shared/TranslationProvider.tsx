c88383d2f31dc3bd3ebe2d6698f0aa5405a6b95c
KSInvictus <kevin.shelton@invictusbpo.com>
2025-05-23 09:52:29 -0500

    Update TranslationProvider.tsx

--- src/components/shared/TranslationProvider.tsx
+++ src/components/shared/TranslationProvider.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState, useEffect, createContext, useContext } from 'react';
+import React, { useState, useEffect, } from 'react';
 import { Globe } from 'lucide-react';
 
 // Available languages
